27892bb4017f7ccf1219d7c643881f2821beda59
ndtai1527 <ductai1527@gmail.com>
2024-03-24 18:38:25 +0700

    :))

--- fvckximi.sh
+++ fvckximi.sh
@@ -73,7 +73,7 @@ framework() {
 
 	lang_dir="$dir/module/lang"
 
-	jar_util d "framework.jar" fw
+	jar_util d "framework.jar" fw classes classes3 
 
 	#patch 
 
@@ -94,7 +94,7 @@ framework() {
 	fi
 
 	
-	jar_util a "framework.jar" fw
+	jar_util a "framework.jar" fw classes classes3 
 }
 
 if [[ ! -d $dir/jar_temp ]]; then
