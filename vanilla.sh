fd525693bd5bf12f23d0e7a17b38154393ee1ab7
ndtai1527 <ductai1527@gmail.com>
2024-06-03 23:01:39 +0700

    ncc

--- vanilla.sh
+++ vanilla.sh
@@ -16,8 +16,8 @@ jar_util() {
         mkdir $dir/jar_temp
     fi
 
-    bak="java -jar $dir/bin/baksmali.jar d --allow-odex-opcodes"
-    sma="java -jar $dir/bin/smali.jar a --allow-odex-opcodes"
+    bak="java -jar $dir/bin/baksmali.jar d"
+    sma="java -jar $dir/bin/smali.jar a"
 
     if [[ $1 == "d" ]]; then
         echo "====> Disassembling $2"
