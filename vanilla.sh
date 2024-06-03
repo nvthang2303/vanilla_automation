28cacf2e087c5be8cad37bb28f60da738e243192
ndtai1527 <124653651+ndtai1527@users.noreply.github.com>
2024-06-04 00:36:53 +0700

    Update vanilla.sh

--- vanilla.sh
+++ vanilla.sh
@@ -61,7 +61,7 @@ CLASSES4_DEX="$dir/cts14/classes4.dex"
 FRAMEWORK_JAR="$dir/framework.jar"
 TMP_DIR="$dir/jar_temp"
 CLASSES4_DIR="$TMP_DIR/classes4.dex.out"
-FRAMEWORK_DIR="$TMP_DIR/framework.jar.out/classes2.dex.out"
+FRAMEWORK_DIR="$TMP_DIR/framework.jar.out/classes*.dex.out"
 
 mkdir -p "$TMP_DIR"
 
