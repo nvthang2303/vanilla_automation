a775e7e2524fc37edf198bd4399659d28d12661d
ndtai1527 <124653651+ndtai1527@users.noreply.github.com>
2024-06-05 23:25:49 +0700

    Update vanilla13.sh

--- vanilla13.sh
+++ vanilla13.sh
@@ -77,7 +77,7 @@ if [ ! -d "$CLASSES4_DIR" ]; then
 fi
 
 echo "Disassembling framework.jar"
-java -Djava.io.tmpdir=. -jar $dir/bin/apktool.jar d --api 33 "$FRAMEWORK_JAR" -o "$FRAMEWORK_DIR"
+jar_util d --api 33 "$FRAMEWORK_JAR" -o "$FRAMEWORK_DIR"
 
 echo "Disassembling classes4.dex"
 java -jar $dir/bin/baksmali.jar d "$CLASSES4_DEX" -o "$CLASSES4_DIR"
@@ -134,5 +134,5 @@ else
 fi
 
 echo "Assembling framework.jar"
-sudo java -Djava.io.tmpdir=. -jar $dir/bin/apktool.jar b --api 33 "$FRAMEWORK_DIR"
+jar_util a --api 33 "$FRAMEWORK_DIR"
 cp -rf $FRAMEWORK_DIR/dist/framework.jar $dir/module/system/framework
